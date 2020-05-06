94cc6bf6c6936bd320f2a00076733ef909d2f853
BenJerry <1092056970@qq.com>
2020-05-06 23:08:22 +0800

    some changes

---
+++ demo1/src/main/java/org/example/lambda/Path.java
@@ -0,0 +1,12 @@
+package org.example.lambda;
+
+import java.nio.file.FileSystems;
+
+
+public interface Path {
+    
+    public static java.nio.file.Path get(String first, String ... more) {
+        return FileSystems.getDefault().getPath(first, more);
+    }
+
+}
